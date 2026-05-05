2205dec401ee7683802d92a154a01838dfe84c15
hotasirogate <hotasimanurung28@gmail.com>
2026-05-06 06:53:57 +0700

    week11: (task) configure Philips WiZ using apply

--- src/oop_00000078570_FaizulAnwar_week11/SmartHomeDashboard.kt
+++ src/oop_00000078570_FaizulAnwar_week11/SmartHomeDashboard.kt
@@ -6,4 +6,23 @@ fun main() {
 
     println("Sistem Smart Home telah diinisialisasi.")
     println("Jumlah perangkat saat ini: ${homeDevices.size}")
+
+    val smartLamp = SmartDevice("", "").apply {
+        // apply digunakan untuk mengatur properti di dalam lingkup objek tersebut
+        name = "Philips WiZ Living Room"
+        category = "Lighting"
+        isOnline = true
+        powerLoad = 12
+    }.also {
+        // also digunakan untuk melakukan aksi tambahan (menambahkan ke list)
+        // setelah konfigurasi selesai
+        homeDevices.add(it)
+        println("Sistem: Perangkat '${it.name}' berhasil ditambahkan ke list.")
+    }
+
+    // Mari kita cek isinya menggunakan fungsi diagnose() yang sudah dibuat sebelumnya
+    println("\n--- Status Perangkat Saat Ini ---")
+    homeDevices.forEach {
+        println(it.diagnose())
+    }
 }
