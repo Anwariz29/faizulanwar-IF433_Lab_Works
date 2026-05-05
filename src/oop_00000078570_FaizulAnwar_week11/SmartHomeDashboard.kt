88d913e38ecfab25e5b5145953efdf28287179ef
hotasirogate <hotasimanurung28@gmail.com>
2026-05-06 06:51:41 +0700

    week11: (task) initialize smart home device list

---
+++ src/oop_00000078570_FaizulAnwar_week11/SmartHomeDashboard.kt
@@ -0,0 +1,9 @@
+package oop_00000078570_FaizulAnwar_week11
+
+fun main() {
+    // Inisialisasi penampung perangkat menggunakan MutableList
+    val homeDevices = mutableListOf<SmartDevice>()
+
+    println("Sistem Smart Home telah diinisialisasi.")
+    println("Jumlah perangkat saat ini: ${homeDevices.size}")
+}
