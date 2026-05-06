5f93d627e83cf4290ccc04487bde0c5c1e1c9db3
hotasirogate <hotasimanurung28@gmail.com>
2026-05-06 07:02:49 +0700

    week11: (task) implement device searcg with let

--- src/oop_00000078570_FaizulAnwar_week11/SmartHomeDashboard.kt
+++ src/oop_00000078570_FaizulAnwar_week11/SmartHomeDashboard.kt
@@ -38,6 +38,18 @@ fun main() {
     // 4. Konfigurasi Alat Pakan Peliharaan
     homeDevices.add(SmartDevice("Picolo's Auto Feeder", "Pet Care", true, 10))
 
+    println("\n--- Hasil Pencarian Aman ---")
+
+    // 1. Mencari perangkat dengan kategori "Camera"
+    val searchResult = homeDevices.find { it.category == "Camera" }
+
+    // 2. Menggunakan ?.let untuk menangani hasil pencarian secara aman
+    searchResult?.let {
+        // Blok ini hanya akan dijalankan jika searchResult TIDAK null
+        println("Hasil ditemukan:")
+        println(it.diagnose())
+    } ?: println("Pencarian Selesai: Perangkat tidak ditemukan.")
+
     // Mari kita cek isinya menggunakan fungsi diagnose() yang sudah dibuat sebelumnya
     println("\n--- Status Perangkat Saat Ini ---")
     homeDevices.forEach {
