761e1fc41d6795f9903ece722e9d2021c7739301
hotasirogate <hotasimanurung28@gmail.com>
2026-05-11 23:15:17 +0700

    week03: (task 2) implement player testing in main

--- src/oop_00000078570_FaizulAnwar_week03/Main.kt
+++ src/oop_00000078570_FaizulAnwar_week03/Main.kt
@@ -12,4 +12,10 @@ fun main() {
 
     val player = Player("Faizul")
 
+    player.addXp(50)
+    println("Level sekarang: ${player.level}")
+
+    player.addXp(60)
+    println("Level sekarang: ${player.level}")
+
 }
