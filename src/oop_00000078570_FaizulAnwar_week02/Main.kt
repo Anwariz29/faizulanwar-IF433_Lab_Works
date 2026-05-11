5ab5352c34c836ef560390712df7b54c494d4527
hotasirogate <hotasimanurung28@gmail.com>
2026-05-11 22:18:22 +0700

    week02: implement user input and prevalidation

---
+++ src/oop_00000078570_FaizulAnwar_week02/Main.kt
@@ -0,0 +1,27 @@
+package oop_00000078570_FaizulAnwar_week02
+
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    println("--- APLIKASI PMB UMN ---")
+
+    print("Masukkan Nama: ")
+    val name = scanner.nextLine()
+
+    print("Masukkan NIM (wajib 5 karakter): ")
+    val nim = scanner.next()
+
+    scanner.nextLine()
+
+    if (nim.length != 5) {
+        println("ERROR: pendaftaran dibatalkan. NIM harus 5 karakter.")
+    } else{
+        print("Masukkan Jurusan: ")
+        val major =  scanner.nextLine()
+
+        val s1 = Student(name, nim, major)
+        println("Status: Pendaftaran Selesai.")
+    }
+}
