3f94009c313bc0113bc500f83d243f7ac86552d6
hotasirogate <hotasimanurung28@gmail.com>
2026-05-13 00:32:15 +0700

    week12: (task) create custom feeder exception subclasses

--- src/oop_00000078570_FaizulAnwar_week12/FeederExceptions.kt
+++ src/oop_00000078570_FaizulAnwar_week12/FeederExceptions.kt
@@ -12,3 +12,35 @@ class OverfeedException :
 
 class EmptyFoodStockException :
     FeederException("Stok makanan habis")
+
+// Custom Exception 1
+class FoodEmptyException(
+    requested: Int,
+    available: Int
+) : FeederException(
+    "Kibble tidak cukup! Diminta $requested gr, sisa $available gr"
+)
+
+// Custom Exception 2
+class DispenserJamException :
+    FeederException("Wadah dispenser tersangkut/macet!")
+
+fun dispenseFood(requested: Int, available: Int) {
+
+    if (requested > available) {
+        throw FoodEmptyException(requested, available)
+    }
+
+    // simulasi dispenser macet
+    throw DispenserJamException()
+}
+
+fun main() {
+
+    try {
+        dispenseFood(500, 200)
+
+    } catch (e: FeederException) {
+        println("Error: ${e.message}")
+    }
+}
