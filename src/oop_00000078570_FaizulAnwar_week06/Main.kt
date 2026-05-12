eaf212ac8403ce00978712d1960b22a69b2d5a02
hotasirogate <hotasimanurung28@gmail.com>
2026-05-12 14:45:36 +0700

    week06: (task) instantiate smart devices in main

--- src/oop_00000078570_FaizulAnwar_week06/Main.kt
+++ src/oop_00000078570_FaizulAnwar_week06/Main.kt
@@ -15,6 +15,25 @@ fun main() {
     val pay1 = Gopay()
     val pay2 = CreditCard()
 
+    val hub = SmartHomeHub()
+
+    // Instansiasi perangkat
+    val lamp = SmartLamp("L001", "Ruang Tamu")
+    val speaker = SmartSpeaker("S001", "Google Nest Dapur")
+    val cctv = SmartCCTV("C001", "Ezviz Garasi")
+
+    // Menambahkan perangkat ke SmartHomeHub
+    hub.addDevice(lamp)
+    hub.addDevice(speaker)
+    hub.addDevice(cctv)
+
+
+    println("\n=== Mengaktifkan Security Mode ===")
+    hub.activateSecurityMode()
+
+    println("\n=== Mematikan Semua Switchable Device ===")
+    hub.turnOffAllSwitches()
+
     println("\n=== TESTING CHECKOUT ===")
     processCheckout(pay1, 50000.0)
     processCheckout(pay2, 150000.0)
