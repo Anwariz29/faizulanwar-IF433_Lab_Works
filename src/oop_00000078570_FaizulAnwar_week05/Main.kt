d5e59e70af65eb8ad3f7081461edcf3405ed4f53
hotasirogate <hotasimanurung28@gmail.com>
2026-05-12 13:48:32 +0700

    week05: implement polymorphic collection and smart casting

---
+++ src/oop_00000078570_FaizulAnwar_week05/Main.kt
@@ -0,0 +1,26 @@
+package oop_00000078570_FaizulAnwar_week05
+
+fun main() {
+    val dosen1 = Dosen("pak Alex", "0123456")
+    val admin1 = Admin("Bu Siti")
+
+    val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
+
+    println("=== AKTIVASI PEGAWAI ===")
+    for (pegawai in daftarPegawai) {
+        pegawai.bekerja()
+
+        when (pegawai) {
+            is Dosen -> {
+                println("=> Terdeteksi sebagai DOsen (NIDN: ${pegawai.nidn}")
+                pegawai.mengajar()
+            }
+            is Admin -> {
+                println("=> Terdeteksi sebagai Admin")
+                pegawai.doAdminWork()
+            }
+        }
+        println("-------------------------")
+    }
+
+}
